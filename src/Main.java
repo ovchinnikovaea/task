3e44c5b50115275e3ff0bc6e87012b7422e4f316
ovchinnikovaea <ov4innikova.e.a@mail.ru>
2023-09-17 17:13:16 +0300

    task is done

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int number = 5;
+        int square = calcSquareOfNumber(number);
+        System.out.println("Квадрат числа " + number + " = " + square);
+
+    }
+
+    public static int calcSquareOfNumber(int number) {
+        return number * number;
+    }
+}
